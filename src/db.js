573ec9bc4d9afd6cbbfdbd68017d24d27f011719
ayushyewle <100531576+ayushyewle@users.noreply.github.com>
2024-02-06 21:23:32 +0530

    Add files via upload

---
+++ src/db.js
@@ -0,0 +1,41 @@
+export const Seed_data_storage_key = "@coding_ninja_ecom_seed_data_key_1413er3";
+export const Cart_storage_key = "@coding_ninja_ecom_cart_storage_key_1413er3";
+export const Dummy_product_list = [
+    {
+        "id": 20,
+        "name": "Iphone 13",
+        "imageUrl": "https://mobilepriceall.com/wp-content/uploads/2022/09/Apple-iPhone-14-Pro-Max.jpg",
+        "price": 59900
+    },
+    {
+        "id": 21,
+        "name": "Samsung S23 ultra",
+        "imageUrl": "https://fdn2.mobgsm.com/vv/pics/samsung/samsung-galaxy-s22-ultra-5g-2.jpg",
+        "price": 117999
+    },
+    {
+        "id": 22,
+        "name": "Blue Jeans Jacket For Mens",
+        "imageUrl": "https://i5.walmartimages.com/asr/d94cdec8-b05b-4da4-b9cc-8c6121361b13_1.7cb6c5ef711cce3754597a09eddb2c7f.jpeg",
+        "price": 4990
+    },
+    {
+        "id": 23,
+        "name": "Grey Jeans Jacket For Women",
+        "imageUrl": "https://i.pinimg.com/736x/99/25/3a/99253a71de190f476d13dfba5cee432d.jpg",
+        "price": 3999
+    },
+    {
+        "id": 24,
+        "name": "Black Jeans Shearling For Mens",
+        "imageUrl": "https://i1.wp.com/www.jacketscreator.com/wp-content/uploads/2018/05/black-shearling-jacket.jpg?fit=1275%2C1500&ssl=1",
+        "price": 9999
+    },
+    {
+        "id": 25,
+        "name": "Men's Warm Thicken Hoodie",
+        "imageUrl": "https://images-na.ssl-images-amazon.com/images/I/71eAaz3svYL._AC_UL1500_.jpg",
+        "price": 2999
+    }
+   
+]
